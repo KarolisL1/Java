f7f4d382645953cda415e9d9406fb767ec292be9
karolisliepa <Karolis.Liepa@gmail.com>
2022-04-02 12:31:36 -0400

    First

---
+++ Java Foundations/First_Java_Program/First.java
@@ -0,0 +1,6 @@
+public class First {
+    public static void main(String[] args) {
+        String newline = System.lineSeparator();
+        System.out.println("My Name is Karolis." + newline + "I am a 99 years old." + newline + "My home town is Philadelphia, PA.");
+    }
+}
