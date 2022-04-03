b720a08495985022aa44e182946e4b8de4df8fb9
karolisliepa <Karolis.Liepa@gmail.com>
2022-04-03 17:45:03 -0400

    finished

--- Java Foundations/CafeBusiness/CafeUtil.java
+++ Java Foundations/CafeBusiness/CafeUtil.java
@@ -19,11 +19,27 @@ public class CafeUtil {
 
     public void displayMenu(ArrayList<String> menu) {
         for (int i = 0; i < menu.size(); i++) {
-            // String value = menu.indexOf(menu, i);
-            // System.out.println(value);
             int post = menu.indexOf(menu.get(i));
-            // System.out.println(post);
             System.out.println(post + " " + menu.get(i));
+        }
+    }
+
+    public void addCustomer(ArrayList<String> customers) {
+        System.out.println("Please enter your name:");
+        String userName = System.console().readLine();
+        System.out.println("Hello " + userName);
+        customers.add(userName);
+        int customervalue = customers.size();
+        System.out.printf("There are %d people in front of you.\n", customervalue);
+        System.out.println(customers);
+    }
+
+    public void printPriceChart(String product, double price, int maxQuantity){
+        // System.out.printf("%s costs $%.2f each.\n", product, price);
+        // System.out.printf("We have %d %s available.\n", maxQuantity, product);
+        System.out.println(product);
+        for(int i =0; i<maxQuantity; i++){
+            System.out.printf("%d - $%.2f\n", i+1, price*(i+1));
         }
     }
 }
--- Java Foundations/CafeBusiness/TestCafe.java
+++ Java Foundations/CafeBusiness/TestCafe.java
@@ -15,11 +15,11 @@ public class TestCafe {
     System.out.println("\n----- Streak Goal Test -----");
     System.out.printf("Purchases needed by week 10: %s \n\n", cafeUtil.getStreakGoal());
     
-        // System.out.println("----- Order Total Test-----");
+    System.out.println("----- Order Total Test-----");
     double[] lineItems = {3.5, 1.5, 4.0, 4.5};
     System.out.printf("Order total: %s \n\n",cafeUtil.getOrderTotal(lineItems));
         
-        // System.out.println("----- Display Menu Test-----");
+    System.out.println("----- Display Menu Test-----");
         
     ArrayList<String> menu = new ArrayList<String>();
     menu.add("drip coffee");
@@ -29,12 +29,15 @@ public class TestCafe {
 
     cafeUtil.displayMenu(menu);
     
-        // System.out.println("\n----- Add Customer Test-----");
-        // ArrayList<String> customers = new ArrayList<String>();
+    System.out.println("\n----- Add Customer Test-----");
+    ArrayList<String> customers = new ArrayList<String>();
         // // --- Test 4 times ---
-        // for (int i = 0; i < 4; i++) {
-        //     appTest.addCustomer(customers);
-        //     System.out.println("\n");
-        // }
+    // for (int i = 0; i < 4; i++) {
+    //     cafeUtil.addCustomer(customers);
+    //     System.out.println("\n");
+    // }
+
+    System.out.println("\n----- Print Price Chart Test-----");
+    cafeUtil.printPriceChart("drip coffee", 15.0, 3);
     }
 }
